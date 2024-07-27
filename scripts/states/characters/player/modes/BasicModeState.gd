58ebfcf73398d1c340327496bcfb69205a320126
daniel-michel <65034538+daniel-michel@users.noreply.github.com>
2024-07-27 18:22:50 +0200

    feat: add player shooting

--- scripts/states/characters/player/modes/BasicModeState.gd
+++ scripts/states/characters/player/modes/BasicModeState.gd
@@ -3,6 +3,16 @@ extends ModeState
 
 # Virtual function. Receives events from the `_unhandled_input()` callback.
 func handle_input(_event: InputEvent) -> void:
+	if player.can_attack:
+		if(Input.is_action_pressed("Attack") and Input.is_action_pressed("down") and !player.is_on_floor()):
+			player.can_attack = false
+			player.anim_tree_playback.travel("DownAttack")				
+		elif (Input.is_action_pressed("Attack") and Input.is_action_pressed("up")):
+			player.can_attack = false
+			player.anim_tree_playback.travel("UpAttack")
+		elif(Input.is_action_just_pressed("Attack")):
+			player.can_attack = false
+			player.anim_tree_playback.travel("Attack")
 	pass
 
 
