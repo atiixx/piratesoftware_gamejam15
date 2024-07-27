cb2974a8df69abd983dcd97274609a05da36d31e
atiixx <jonas.hauenstein@outlook.de>
2024-07-27 16:22:39 +0200

    feat: added moving enemy

--- scripts/character_parent.gd
+++ scripts/character_parent.gd
@@ -1,11 +1,13 @@
 extends Node2D
 
 @onready var player: CharacterBody2D = find_child("Player")
-@onready var boss: CharacterBody2D = find_child("RangedBoss")
+@onready var children: Array[Node] = get_children()
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	player.enemy_hit.connect(_on_enemy_hit)
-	boss.player_hit.connect(_on_player_hit)
+	for child in children:
+		if child != player and child.has_signal("player_hit"):
+			child.player_hit.connect(_on_player_hit)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
---
+++ scripts/enemy/moving_enemy.gd
@@ -0,0 +1,34 @@
+extends Enemy
+
+@export var walking_speed = 400
+var direction = 1
+@onready var ledge_detector: RayCast2D = $LedgeDetector
+@onready var wall_detector: RayCast2D = $WallDetector
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	sprite.flip_h = velocity.x < 0
+	if !ledge_detector.is_colliding() or is_on_obstacle():
+		ledge_detector.position.x *= -1
+		wall_detector.target_position.x *= -1
+		direction *= -1
+	
+func _physics_process(delta):
+	velocity.y += gravity * delta
+	velocity.x = walking_speed * direction
+	move_and_slide()
+
+func is_on_obstacle():
+	if wall_detector.is_colliding():
+		return true			
+	return false
+	
+	
+func _on_hurtbox_body_entered(body: Node2D):
+	var player_layer = body.get_collision_layer_value(2)
+	if player_layer:
+		player_hit.emit(self)
