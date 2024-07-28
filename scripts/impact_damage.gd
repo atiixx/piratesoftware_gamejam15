6c79e3bb4971b70fe607c49334e801dc23b9f614
daniel-michel <65034538+daniel-michel@users.noreply.github.com>
2024-07-28 10:42:29 +0200

    fix: projectile cannot make damage before shot

--- scripts/impact_damage.gd
+++ scripts/impact_damage.gd
@@ -11,9 +11,7 @@ func _on_body_entered(body: Node2D):
 	if not active:
 		return
 	var correct_layer = (body as CollisionObject2D).collision_mask & damage_matching
-	print("layer: ", correct_layer, " has method: ", body.has_method("get_hit"))
 	if correct_layer and body.has_method("get_hit"):
-		print("applying damage")
 		var source_node = source if source else owner
 		if body is Player:
 			body.get_hit(source_node)
